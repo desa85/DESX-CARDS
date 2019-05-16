bc2a0aca5b09ddc46a21112d6f661185f0fcadf6
desa85 <85desa@gmail.com>
2019-05-16 21:09:56 +0300

    adaptive to babel

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,34 +1,50 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
 module.exports = {
   entry: "./src/index.tsx",
   output: {
-      filename: "bundle.js",
-      path: __dirname + "/dist"
-  },
+		filename: "bundle.js",
+		path: path.resolve(__dirname, './dist')
+	},
+	
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'DESX-CARDS',
+			template: "./src/index.html"
+		})
+	],
 
-  // Enable sourcemaps for debugging webpack's output.
   devtool: "source-map",
 
   resolve: {
-      // Add '.ts' and '.tsx' as resolvable extensions.
-      extensions: [".ts", ".tsx", ".js", ".json"]
+		extensions: [".ts", ".tsx", ".js", ".json"]
   },
 
   module: {
-      rules: [
-          // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-          { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-
-          // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-          { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-      ]
+		rules: [
+			{
+        test: /\.tsx$/,
+        loader: "babel-loader"
+			}
+		]
   },
 
-//   When importing a module whose path matches one of the following, just
-//   assume a corresponding global variable exists and use that instead.
-//   This is important because it allows us to avoid bundling all of our
-//   dependencies, which allows browsers to cache those libraries between builds.
   externals: {
-      "react": "React",
-      "react-dom": "ReactDOM"
+		"react": "React",
+		"react-dom": "ReactDOM"
+	},
+
+	watchOptions: {
+    poll: true,
+    ignored: /node_modules/
+  },
+	
+	devServer: {
+    contentBase: 'src',
+    inline: true,
+    hot: true,
+    open: true,
+    port: 3000
   }
 };
