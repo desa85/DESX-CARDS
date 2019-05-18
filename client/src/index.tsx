ac53d3416ec45e7706ae426c63ddfe918202409d
desa85 <85desa@gmail.com>
2019-05-18 23:43:10 +0300

    Create main maket

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,9 +2,9 @@ import './styles/main.scss';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import { Hello } from "./components/hello";
+import { App } from "./components/hello";
 
 ReactDOM.render(
-  <Hello compiler = "Typescript" framework = "React" />,
+  <App compiler = "Typescript" framework = "React" />,
   document.getElementById("example")
 )
