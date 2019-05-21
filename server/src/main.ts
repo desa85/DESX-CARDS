388d45c133b949230b0eceb4bba69f91aaa4af84
desa85 <85desa@gmail.com>
2019-05-21 19:22:04 +0300

    Add generate data

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,7 @@ import {QueryResult, Client} from "pg";
 import { Request, Response, Application } from 'express'
 
 const express = require('express')
+const uuid = require('uuid/v4')
 
 const client = new Client({
   user: 'desx_user',
@@ -11,15 +12,39 @@ const client = new Client({
   port: 5430,
 })
 
-client.connect()
-client.query(
-  `CREATE TABLE IF NOT EXISTS cards (
-    id uuid not null primary key, 
-    name varchar(16) not null, 
-    typeMagic varchar(16) not null, 
-    power int not null
-  )`
-)
+async function createTable() {
+  let row
+
+  await client.connect()
+
+  await client.query(
+    `CREATE TABLE IF NOT EXISTS cards (
+      id uuid not null primary key, 
+      name varchar(16) not null, 
+      typeMagic varchar(16) not null, 
+      power int not null
+    )`
+  )
+
+  row = await client.query(`SELECT COUNT(*) FROM cards`)
+  try {
+    if (row.rows[0].count == 0) {
+      await client.query(
+        `insert into cards (id, name, typeMagic, power) values
+        ('${uuid()}', 'АНТАРАС', 'ЗЕМЛЯ', 60),
+        ('${uuid()}', 'СОНИК', 'ВОДА', 55),
+        ('${uuid()}', 'ГЛЫБА', 'ЗЕМЛЯ', 30),
+        ('${uuid()}', 'ХЬЮГА', 'ВЕТЕР', 50),
+        ('${uuid()}', 'ВАЛАКАС', 'ОГОНЬ', 70);`
+      )
+
+    }
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+createTable()
 
 const app = express()
 
