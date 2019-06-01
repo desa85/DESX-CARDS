45f536ab8ea0f75471c58f0f0fe21703f09181d0
desa85 <85desa@gmail.com>
2019-06-02 02:27:03 +0300

    Change PR: refactoring

--- server/src/main.ts
+++ server/src/main.ts
@@ -60,49 +60,58 @@ app.get('/api/card/list', (req: Express.Request, res: Express.Response) => {
 })
 
 app.post('/api/card/', (req: Express.Request, res: Express.Response) => {
-  let isSuccess = true;
-  let code: string, 
+  interface Error {code: string; message: string};
+
+  class ErrorMaker {
+    code: string
     message: string
+    constructor(code: string, message: string) {
+      this.code = code
+      this.message = message
+    }
+  }
 
+  const id: string = uuid()
+  const validationLine = ([name, type]: string[]): Error | null => {
+    if (!req.body[name]) {
+      return new ErrorMaker('no-line', `No property ${name}`)
+    } else if (typeof req.body[name] !== type) {
+      return new ErrorMaker('disparity-type', `wrong type in ${name}`)
+    } else if (name === 'name' && req.body[name] && req.body[name].length > 6) {
+      return new ErrorMaker('validation-name', `In the '${name}' property, more than 6 characters`)
+    } else if (name === 'typeMagic' && !['earth', 'water', 'fire', 'wind'].includes(req.body[name])) {
+      return new ErrorMaker('validation-type-magic', `must be enum type`)
+    } else if (name === 'power' && (req.body[name] < 1 || req.body[name] > 200)) {
+      return new ErrorMaker('validation-power', `value is not valid`)
+    } else {
+      return null
+    }
+  }
+  const nameLine: string[][] =
   [
-    ['string', 'name'],
-    ['string', 'typeMagic'],
-    ['number', 'power']
-  ].forEach(function ([type, name]) {
+    ['name', 'string'],
+    ['typeMagic', 'string'],
+    ['power', 'number']
+  ]
 
-    function Possible(option: string): boolean {
-      return option === req.body[name]
-    }
+  const searchErr = (arr: string[][], call: Error | null, index: number = 0): Error | null => {
+    const length: number = arr.length
+    if (index <= length - 1) {
+      const result: Error | null = call(arr[index])
+      if (result) return result
+      else return searchErr(arr, call, ++index)
+    } else return null
+  }
 
-    if (isSuccess) {
-      if (!req.body[name]) [code, message] = ['no-line', `No property ${name}`]
-      else if (typeof req.body[name] !== type) [code, message] = ['disparity-type', `wrong type in ${name}`]
-      else if (name === 'name' && req.body[name] && req.body[name].length > 6) {
-        [code, message] = ['validation-name', `In the '${name}' property, more than 6 characters`]
-      } else if (['earth', 'water', 'fire', 'wind'].find(Possible)) {
-        [code, message] = ['validation-type-magic', `must be enum type`]
-      } else if (name === 'power' && (req.body[name] < 1 || req.body[name] > 200)) {
-        [code, message] = ['validation-power', `value is not valid`]
-      }
-    }
-    
-    if (code) {
-      isSuccess = false
-      res.status(400).send({
-        code: code,
-        message: message
-      })
-    }
-  } )
+  const err = searchErr(nameLine, validationLine)
 
-  if (isSuccess) {
-    const id: string = uuid()
+  if (!err) {
     client.query(`INSERT INTO cards (id, name, type_magic, power) VALUES 
-      ('${id}', '${req.body.name}', '${req.body.typeMagic}', ${req.body.power})`)
+    ('${id}', '${req.body.name}', '${req.body.typeMagic}', ${req.body.power})`)
       .then(() => {
         res.send({ id: id, name: req.body.name, typeMagic: req.body.typeMagic, power: req.body.power })
       })
-  }
+  } else res.status(400).send(err)
 })
 
 app.delete('/api/card/:id', (req: Express.Request, res: Express.Response) => {
